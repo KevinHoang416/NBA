6563e769216b8eb7279ce3c8e766bdceab7a2b11
Kevin Hoang <kevinhoang@Kevins-MacBook-Air.local>
2022-04-12 22:29:14 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
